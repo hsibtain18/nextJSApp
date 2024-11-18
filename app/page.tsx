bb6837a62ba4f6a32178a3bd745c4496615dab4c
hsibtain18 <Hsibtain18@gmail.com>
2024-11-18 18:44:44 +0500

    assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="  grid min-h-screen grid-rows-[20px_1fr_20px] items-center  justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] shadow-dark-200 sm:p-20">
-     
+     <Image src="/icons/close.svg" alt="" width='100' height='100' />
      <h1 className="font-black " > adafaudhfaidvandfovnadvjnadivojnadvtemp</h1>
      <h1 className="font-space-grotesk" > adafaudhfaidvandfovnadvjnadivojnadvtemp</h1>
     </div>
